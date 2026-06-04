9dd97ff72581a1c097e4921415d8f47416391862
Syed Aejaz Ahmed A <syed.aejaz.ahmed2006@gmail.com>
2026-06-04 13:27:34 +0530

    Time: 3 ms (18.60%) | Memory: 48 MB (6.23%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -9,10 +9,10 @@ class Solution {
             }else if(nums[i]==0 && nums[j]!=0){
                 nums[i] = nums[j];
                 nums[j] = 0;
+                i+=1;
+                j+=1;
             }else if(nums[i]==0 && nums[j]==0){
                 j+=1;
-            }else if(nums[i]!=0 && nums[j]==0){
-                i+=1;
             }
         }
     }
